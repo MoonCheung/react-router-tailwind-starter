1501e7d28c3f0af23228ecf3920e2a3a0b57d142
MoonCheung <ikmoons.is@gmail.com>
2025-05-03 22:31:21 +0800

    chore: tailwindcss v3 to v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
 });
