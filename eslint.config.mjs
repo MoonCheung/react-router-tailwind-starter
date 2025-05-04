655e7127254bb5c55d7c7696668fe4b52c1966ec
MoonCheung <ikmoons.is@gmail.com>
2025-05-04 15:50:20 +0800

    chore: update prettier or eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,40 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import path from 'path'
+import reactPlugin from 'eslint-plugin-react'
+import { includeIgnoreFile } from '@eslint/compat'
+import globals from 'globals'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+const gitignorePath = path.resolve(import.meta.dirname, '.gitignore')
+
+export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
+  eslint.configs.recommended,
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  },
+  reactPlugin.configs.flat.recommended, // This is not a plugin object, but a shareable config object
+  reactPlugin.configs.flat['jsx-runtime'],
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  }
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from 'vite'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { reactRouter } from '@react-router/dev/vite'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   css: {
     postcss: {
-      plugins: [autoprefixer],
-    },
+      plugins: [autoprefixer]
+    }
   },
-  plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
-});
+  ssr: {
+    noExternal: command === 'build' ? true : undefined
+  },
+  plugins: [reactRouter(), tsconfigPaths(), tailwindcss()]
+}))
