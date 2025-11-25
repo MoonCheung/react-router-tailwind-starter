692e5122a187892ce7410897f024d74ca08f14aa
MoonCheung <ikmoons.is@gmail.com>
2025-11-26 00:04:24 +0800

    feat: add skeleton components

---
+++ app/components/shadcn/ui/skeleton.tsx
@@ -0,0 +1,13 @@
+import { clsxm } from '~/lib/utils';
+
+function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={clsxm('bg-primary/10 animate-pulse rounded-md', className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
