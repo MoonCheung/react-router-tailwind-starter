beda8715af03c19ab071117c8c402943ee99cd87
MoonCheung <ikmoons.is@gmail.com>
2026-01-05 23:54:09 +0800

    feat: graphql modules folder

---
+++ app/routes/__.demo.graphql._index/route.tsx
@@ -0,0 +1,11 @@
+import type { Route } from './+types/route';
+
+const GraphqlPage = () => {
+  return (
+    <>
+      <div>hello Graphql Page</div>
+    </>
+  );
+};
+
+export default GraphqlPage;
